e41efa1bb243836b6e77e14b3a6827a987c580bd
Jack Badger <jackbadger16@gmail.com>
2024-06-24 19:59:39 +0100

    changes to persist storare

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import { createStore, combineReducers } from "redux";
+import { PersistGate } from "redux-persist/integration/react";
 
+// import { persistStore, persistReducer } from "redux-persist";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </Provider>,
+    <PersistGate loading={null}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
-
-
