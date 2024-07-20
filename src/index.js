1e12de6fdd1cb1836afcd154da7adc244a05ec4b
Jack Badger <jackbadger16@gmail.com>
2024-07-20 19:31:06 +0100

    removing redux

--- src/index.js
+++ src/index.js
@@ -3,20 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-import { createStore, combineReducers } from "redux";
-import { PersistGate } from "redux-persist/integration/react";
-
-// import { persistStore, persistReducer } from "redux-persist";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <PersistGate loading={null}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 );
